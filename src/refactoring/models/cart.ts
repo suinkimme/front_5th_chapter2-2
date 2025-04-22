aa1431249f0e0c0d32d2c53a2edd2de3cd590386
Suin Kim <contact@suinkim.me>
2025-04-22 21:33:27 +0900

    refactor: model cart 순수 함수 분리

--- src/refactoring/models/cart.ts
+++ src/refactoring/models/cart.ts
@@ -1,7 +1,26 @@
 import { CartItem, Coupon, Product } from "../../types";
 
-export const calculateItemTotal = (item: CartItem, discount: number): number =>
-  item.product.price * item.quantity * (1 - discount);
+// CartPage.js에 있는 getMaxDiscount로 계산 함수 분리해도 좋을 것 같음
+export const calculateItemTotal = (item: CartItem) => {
+  const discount = getMaxApplicableDiscount(item);
+  return item.product.price * item.quantity * (1 - discount);
+};
+
+export const calculateTotalWithDiscount = (cart: CartItem[]) => {
+  const totalBeforeDiscount = cart.reduce(
+    (total, item) => total + item.product.price * item.quantity,
+    0
+  );
+  const totalAfterDiscount = cart.reduce(
+    (total, item) => total + calculateItemTotal(item),
+    0
+  );
+
+  return {
+    totalBeforeDiscount,
+    totalAfterDiscount,
+  };
+};
 
 export const getMaxApplicableDiscount = (item: CartItem) =>
   item.product.discounts.reduce(
@@ -12,37 +31,25 @@ export const getMaxApplicableDiscount = (item: CartItem) =>
     0
   );
 
+export const caclulateTotalDiscount = (
+  totalAfterDiscount: number,
+  selectedCoupon: Coupon
+) =>
+  selectedCoupon.discountType === "amount"
+    ? Math.max(0, totalAfterDiscount - selectedCoupon.discountValue)
+    : totalAfterDiscount * (1 - selectedCoupon.discountValue / 100);
+
 export const calculateCartTotal = (
   cart: CartItem[],
   selectedCoupon: Coupon | null
 ) => {
-  let totalBeforeDiscount = 0;
-  let totalAfterDiscount = 0;
+  console.log(selectedCoupon);
 
-  cart.forEach((item) => {
-    const { price } = item.product;
-    const { quantity } = item;
-    totalBeforeDiscount += price * quantity;
-
-    const discount = getMaxApplicableDiscount(item);
-
-    totalAfterDiscount += calculateItemTotal(item, discount);
-  });
-
-  let totalDiscount = totalBeforeDiscount - totalAfterDiscount;
-
-  // 쿠폰 적용
-  if (selectedCoupon) {
-    if (selectedCoupon.discountType === "amount") {
-      totalAfterDiscount = Math.max(
-        0,
-        totalAfterDiscount - selectedCoupon.discountValue
-      );
-    } else {
-      totalAfterDiscount *= 1 - selectedCoupon.discountValue / 100;
-    }
-    totalDiscount = totalBeforeDiscount - totalAfterDiscount;
-  }
+  const { totalBeforeDiscount, totalAfterDiscount } =
+    calculateTotalWithDiscount(cart);
+  const totalDiscount = selectedCoupon
+    ? caclulateTotalDiscount(totalAfterDiscount, selectedCoupon)
+    : totalBeforeDiscount - totalAfterDiscount;
 
   return {
     totalBeforeDiscount: Math.round(totalBeforeDiscount),
