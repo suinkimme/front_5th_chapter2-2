402f66b98cb84094df7607167e2d28dc0744d0b9
Suin Kim <contact@suinkim.me>
2025-04-23 01:39:19 +0900

    feat: useProdcuts 기능 작성

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,9 +2,21 @@ import { useState } from "react";
 import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState(initialProducts);
+
+  const updateProduct = (product: Product) => {
+    setProducts((prevProducts) =>
+      prevProducts.map((item) => (item.id === product.id ? product : item))
+    );
+  };
+
+  const addProduct = (product: Product) => {
+    setProducts((prevProducts) => [...prevProducts, product]);
+  };
+
   return {
-    products: initialProducts,
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products,
+    updateProduct,
+    addProduct,
   };
 };
