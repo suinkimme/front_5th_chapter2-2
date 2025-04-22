4c295f8bcf4d5b524d534875d91584ab97835415
Suin Kim <contact@suinkim.me>
2025-04-23 01:41:40 +0900

    feat: useCoupons 기능 작성

--- src/refactoring/hooks/useCoupon.ts
+++ src/refactoring/hooks/useCoupon.ts
@@ -2,5 +2,11 @@ import { useState } from "react";
 import { Coupon } from "../../types.ts";
 
 export const useCoupons = (initialCoupons: Coupon[]) => {
-  return { coupons: initialCoupons, addCoupon: () => undefined };
+  const [coupons, setCoupons] = useState(initialCoupons);
+
+  const addCoupon = (coupon: Coupon) => {
+    setCoupons((prevCoupons) => [...prevCoupons, coupon]);
+  };
+
+  return { coupons, addCoupon };
 };
